ae75ef69abc10d10a400139898a1547cc6840137
Jason Kuhrt <jasonkuhrt@me.com>
2023-03-03 15:04:11 -0500

    make clear

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,15 +1,19 @@
 import { PrismaClient } from '@prisma/client/edge'
 
-export const onRequest: PagesFunction = async (ctx) => {
+export const onRequest = async (ctx: any) => {
   console.log(ctx.env)
 
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: ctx.env.DATABASE_URL_PROXY,
+  try {
+    new PrismaClient({
+      datasources: {
+        db: {
+          url: ctx.env.DATABASE_URL_PROXY,
+        },
       },
-    },
-  })
+    })
+  } catch (e: any) {
+    return new Response(e.message)
+  }
 
   return new Response(`ok`)
 }
